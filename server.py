1f149e06aca9f03021e968e389b64e7420df30c0
Confy-Code <isingizweconfy@gmail.com>
2025-02-08 16:25:01 +0100

    Added progects

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from weather import get_city
+from waitress import serve
+import os
+app= Flask(__name__,)
+@app.route('/')
+@app.route("/index")
+def index():
+    #print(os.listdir("templates"))  # List files in the templates folder
+    return render_template("index.html")
+@app.route("/weather")
+def get_weather():
+    city=request.args.get('city')
+    #empty spaces
+    if not bool(city.strip()):
+        city="Kigali"
+    weather_app=get_city(city)
+    #if not found
+    if not weather_app["cod"]==200:
+        return render_template("city_not_found.html")
+    return render_template("weather.html",title=weather_app["name"],status=weather_app["weather"][0]["description"].capitalize(),
+                           temp=f"{weather_app['main']['temp']:.1f}",
+                           feels_like=f"{weather_app['main']['feels_like']:.1f}"
+                           )
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8000)
---
+++ weather.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+from pyinputplus import inputStr
+load_dotenv()
+
+def get_city(city='Kigali city'):
+    request_url= f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_Key")}&q={city}&units=metric'
+    weather_app=requests.get(request_url).json()
+    #print(weather_app)
+    return weather_app
+
+if __name__=="__main__":
+    print("\nGet our conditions")
+    city=input("\nPut the city name: ")
+    #empty spaces
+    if not bool(city.strip()):
+        city="Kigali"
+    weather_app=get_city(city)
+    print("\n")
+    pprint(weather_app)    
